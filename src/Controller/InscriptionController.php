d98f686e7070dd3a03b8b1c1a450087d29aa1904
Valentin Perrin <valentin.g-a.perrin@outlook.fr>
2019-02-21 22:59:19 +0100

    Design of subscribePage - init homePage and base.html.twig - basic routing available

---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valen
+ * Date: 21/02/2019
+ * Time: 21:31
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InscriptionController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="inscriptionPage")
+     */
+    public function read()
+    {
+        //return new Response('OMG! My first Symfony page! :D');
+
+        return $this->render('inscription/inscription.html.twig');
+    }
+}
