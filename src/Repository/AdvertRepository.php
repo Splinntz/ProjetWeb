abd86e31dcb8f2a890da77cdeb1154e27e85aef4
Rektibou <33221700+Rektibou@users.noreply.github.com>
2019-02-24 16:55:35 +0100

    Ajout fonctionnalité creation annonce
    Ajout d'un bouton sur la page principale permettant a l'utilisateur de créer une annonce. Cela va le guider vers un formulaire création d'annonces.

--- src/Repository/AdvertRepository.php
+++ src/Repository/AdvertRepository.php
@@ -19,6 +19,8 @@ class AdvertRepository extends ServiceEntityRepository
         parent::__construct($registry, Advert::class);
     }
 
+<<<<<<< HEAD
+=======
     // /**
     //  * @return Advert[] Returns an array of Advert objects
     //  */
@@ -47,4 +49,5 @@ class AdvertRepository extends ServiceEntityRepository
         ;
     }
     */
+>>>>>>> e88f2f539204e84a0cc7a53fc79e51c16b0fc3d0
 }
