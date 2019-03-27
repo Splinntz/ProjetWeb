1f06fa45e4de28752b7283d7e9f8c0ffdaf17978
Splinntz <antonin.chigot@gmail.com>
2019-03-27 16:53:34 +0100

    able to answer message in the same page

--- src/Controller/TchatMessageController.php
+++ src/Controller/TchatMessageController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\TchatMessage;
+use App\Form\TchatMessageType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class TchatMessageController extends AbstractController
@@ -25,14 +27,29 @@ class TchatMessageController extends AbstractController
      * @param $tchat_message
      * @return Response
      */
+
     public function create(Request $request, ObjectManager $objectManager, TokenStorageInterface $token)
     {
+        $tchatMessage = new TchatMessage();
 
+        $form = $this->createForm(TchatMessageType::class, $tchatMessage);
+
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tchatMessage->setNameUserName($token->getToken()->getUser()->getLogin());
+            $date = new \DateTime();
+            $date->setTimestamp(123456);
+            $tchatMessage->setDate($date);
+            $tchatMessage->setIdTchatId(1);
+            $objectManager->persist($tchatMessage);
+            $objectManager->flush();
+        }
         return $this->render('tchat/tchat.html.twig',[
-            'tchats_messages' => $this->getDoctrine()->getRepository(TchatMessage::class)->findAll()
+            'tchats_messages' => $this->getDoctrine()->getRepository(TchatMessage::class)->findAll(),
+            'form' => $form->createView()
             ]);
     }
-
-    
-}
-    
+}    
